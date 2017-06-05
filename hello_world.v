983a6467fc0702f6e0e83761ad23822dd8c32ddc
SajeedShahriat <sajeed.shahriat@gmail.com>
2017-06-05 19:37:15 -0400

    First commit

---
+++ hello_world.v
@@ -0,0 +1,16 @@
+/*
+Author: Sajeed Mohammad Shahriat
+Affiliation: Rochester Institute of Technology
+All rights reserved
+This files can be reused and modified given that this copyright notice is not removed
+*/
+
+module hello_world;
+
+initial
+	begin
+		$display ("Hello World");
+		#10
+		$finish;
+	end
+endmodule
