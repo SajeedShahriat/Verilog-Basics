b1cde89208fa8de3848f57055f1653b1586c5769
SajeedShahriat <sajeed.shahriat@gmail.com>
2017-07-19 16:35:13 -0400

    removed empty files

--- 1-bit-half_subractor/half_subtractor.v
+++
@@ -1,6 +0,0 @@
-/*
-Author: Sajeed Mohammad Shahriat
-Affiliation: Rochester Institute of Technology
-All rights reserved
-This files can be reused and modified given that this copyright notice is not removed
-*/
